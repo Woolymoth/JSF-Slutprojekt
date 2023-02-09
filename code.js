0e6211283196d0ea3e95766d5d90ab68ff0244de
Elias Linderos <eliaslinderos@live.se>
2023-02-09 15:51:35 +0100

    added functions fo fetch random parts of the api by user input.

--- code.js
+++ code.js
@@ -1,46 +1,175 @@
-const charUrl = "https://lotrapi.co/api/v1/characters";
-
-const cityUrl="https://lotrapi.co/api/v1/cities";
-
-
-function fetchChar() {
-    fetch(charUrl)
-    .then((response) =>  {
+var charUrl = "https://lotrapi.co/api/v1/characters";
+var cityUrl = "https://lotrapi.co/api/v1/cities";
+function fetchSelect(thirdUrl, random) {
+    fetch(thirdUrl + random)
+        .then(function (response) {
         if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error("NETWORK RESPONSE ERROR");
+            return response.json();
         }
-      })
-    //.then((data) => console.log(data.results[0]))
-    .then((data) => {
-        displayChar(data)
+        else {
+            throw new Error("NETWORK RESPONSE ERROR");
+        }
     })
-};
-
-function displayChar(data) {
-    let myChar = data.results[1];
-    let tab = 
-    `<tr>
-      <th>Name</th>
-      <th>Eye Color</th>
-      <th>Date of Birth</th>
-      <th>Eye Color</th>
-     </tr>`;
-     //@ts-ignore
-     const charSpace = document.getElementById('info').innerHTML += tab +=
-    `<tr>
-    <td>${myChar.name}</td>
-    <td>${myChar.eye_color}</td>
-    <td>${myChar.date_of_birth}</td> 
-    <td>${myChar.eye_color}</td> 
-    </tr>`
-};
-function samFo(event){
-    fetchChar();
+        .then(function (data) {
+        displayUserChoice(data);
+    });
+}
+function displayUserChoice(data) {
+    var element = document.querySelector("tbody");
+    element.remove();
+    var userChoise = data;
+    var tab = "<tr>\n    <th>Title</th>\n   </tr>";
+    const infoSpace = document.getElementById('info').innerHTML += tab +=
+    "<tr>\n  <td>".concat(data.title, "</td>\n  </tr>");
+    console.log(userChoise);
+}
+;
+function selectChoice(event){
+    var yourSelect = document.getElementById(event.target.id);
+    let thirdUrl;
+    let random;
+    switch(yourSelect.options[yourSelect.selectedIndex].id){
+        case "books":
+        thirdUrl= "https://lotrapi.co/api/v1/books/";
+        random= Math.floor(Math.random() * 4) +1; 
+        break; 
+        case "films":
+        thirdUrl= "https://lotrapi.co/api/v1/films/";
+        random= Math.floor(Math.random() * 6) +1;
+        break;
+        }
+    fetchSelect(thirdUrl, random);
     event.document
     event.preventDefault();
+};
+let select = document.querySelector('#userChoice');
+select.addEventListener('change', selectChoice);
+
+function fetchChar(charId) {
+    fetch(charUrl)
+        .then(function (response) {
+        if (response.ok) {
+            return response.json();
+        }
+        else {
+            throw new Error("NETWORK RESPONSE ERROR");
+        }
+    })
+        .then(function (data) {
+        displayChar(data, charId);
+    });
 }
-const samButton = document.querySelector('#Sam');
-console.log(samButton);
-samButton.addEventListener('click', samFo);
+;
+function displayChar(data, charId) {
+    var element = document.querySelector("tbody");
+    element.remove();
+    var myChar = data.results[charId];
+    var tab = "<tr>\n      <th>Name</th>\n      <th>Hair coloe</th>\n      <th>Date of Birth</th>\n      <th>Eye Color</th>\n     </tr>";
+    const infoSpace = document.getElementById('info').innerHTML += tab +=
+    "<tr>\n    <td>".concat(myChar.name, "</td>\n    <td>").concat(myChar.hair_color, "</td>\n    <td>").concat(myChar.date_of_birth, "</td> \n    <td>").concat(myChar.eye_color, "</td> \n    </tr>");
+}
+;
+function charButton(event) {
+    var charId;
+    switch (event.target.name) {
+        case "Frodo":
+            charId = 0;
+            break;
+        case "Sam":
+            charId = 1;
+            break;
+        case "Gandalf":
+            charId = 2;
+            break;
+        case "Aragorn":
+            charId = 4;
+            break;
+        case "Legolas":
+            charId = 7;
+            break;
+        case "Gimli":
+            charId = 8;
+            break;
+        case "Merry":
+            charId = 5;
+            break;
+        case "Pippin":
+            charId = 6;
+            break;
+        case "Boromir":
+            charId = 9;
+            break;
+    }
+    fetchChar(charId);
+    event.document;
+    event.preventDefault();
+}
+document.querySelector('#Frodo').addEventListener('click', charButton);
+document.querySelector('#Sam').addEventListener('click', charButton);
+document.querySelector('#Gandalf').addEventListener('click', charButton);
+document.querySelector('#Aragorn').addEventListener('click', charButton);
+document.querySelector('#Legolas').addEventListener('click', charButton);
+document.querySelector('#Gimli').addEventListener('click', charButton);
+document.querySelector('#Merry').addEventListener('click', charButton);
+document.querySelector('#Pippin').addEventListener('click', charButton);
+document.querySelector('#Boromir').addEventListener('click', charButton);
+function fetchCity(cityId) {
+    fetch(cityUrl)
+        .then(function (response) {
+        if (response.ok) {
+            return response.json();
+        }
+        else {
+            throw new Error("NETWORK RESPONSE ERROR");
+        }
+    })
+        .then(function (data) {
+        displayCity(data, cityId);
+    });
+}
+;
+function displayCity(data, cityId) {
+    var element = document.querySelector("tbody");
+    element.remove();
+    var theCity = data.results[cityId];
+    var tab = "<tr>\n    <th>Name</th>\n    <th>Founding</th>\n    <th>Location</th>\n    <th>Inhabitants</th>\n   </tr>";
+    const infoSpace = document.getElementById('info').innerHTML += tab +=
+    "<tr>\n  <td>".concat(theCity.name, "</td>\n  <td>").concat(theCity.founded_date, "</td>\n  <td>").concat(theCity.location, "</td> \n  <td>").concat(theCity.inhabitants, "</td> \n  </tr>");
+}
+;
+function cityButton(event) {
+    var cityId;
+    switch (event.target.name) {
+        case "Minas Tirith":
+            cityId = 6;
+            break;
+        case "Annúminas":
+            cityId = 0;
+            break;
+        case "Fornost":
+            cityId = 1;
+            break;
+        case "Michel Delving":
+            cityId = 2;
+            break;
+        case "Valmar":
+            cityId = 3;
+            break;
+        case "Elvenking's Halls":
+            cityId = 4;
+            break;
+        case "Erebor":
+            cityId = 5;
+            break;
+    }
+    fetchCity(cityId);
+    event.document;
+    event.preventDefault();
+}
+document.querySelector('#MT').addEventListener('click', cityButton);
+document.querySelector('#Annu').addEventListener('click', cityButton);
+document.querySelector('#Fornost').addEventListener('click', cityButton);
+document.querySelector('#MD').addEventListener('click', cityButton);
+document.querySelector('#Valmar').addEventListener('click', cityButton);
+document.querySelector('#EH').addEventListener('click', cityButton);
+document.querySelector('#Erebor').addEventListener('click', cityButton);
--- code.ts
+++ code.ts
@@ -1,9 +1,65 @@
 const charUrl = "https://lotrapi.co/api/v1/characters";
 
 const cityUrl="https://lotrapi.co/api/v1/cities";
-let x : Number;
 
-function char() {
+function fetchSelect(thirdUrl : any,random : number){
+    fetch(thirdUrl + random)
+    .then((response) =>{
+        if (response.ok){
+            return response.json();
+        } else {
+            throw new Error("NETWORK RESPONSE ERROR");
+        }
+    })
+    .then((data) =>{
+        displayUserChoice(data)
+    })
+}
+
+function displayUserChoice(data :any){
+    const element : any = document.querySelector("tbody");
+    element.remove();
+    let userChoise = data;
+    let tab = `<tr>
+    <th>Title</th>
+   </tr>`;
+   //const infoSpace = document.getElementById('info').innerHTML += tab +=
+  `<tr>
+  <td>${data.title}</td>
+  </tr>`
+     console.log(userChoise)
+};
+
+
+//function selectChoice(event :any){
+    //var yourSelect = document.getElementById(event.target.id);
+    //let thirdUrl;
+    //let random : any;
+    //switch(yourSelect.options[yourSelect.selectedIndex].id){
+        //case "books":
+           // thirdUrl= "https://lotrapi.co/api/v1/books/";
+            //random= Math.floor(Math.random() * 4) +1; 
+            //break; 
+        //case "films":
+           //thirdUrl= "https://lotrapi.co/api/v1/films/";
+           //random= Math.floor(Math.random() * 6) +1;
+           // break;
+    //}
+    //fetchSelect(thirdUrl, random);
+    //event.document
+   // event.preventDefault();
+//};
+
+//let select = document.querySelector('#userChoice');
+//select.addEventListener('change', selectChoice);
+
+
+
+/////////////////////////////////////////////////////////////////////////////////7
+
+
+
+function fetchChar(charId : any) {
     fetch(charUrl)
     .then((response) =>  {
         if (response.ok) {
@@ -12,44 +68,141 @@ function char() {
           throw new Error("NETWORK RESPONSE ERROR");
         }
       })
-    //.then((data) => console.log(data.results[0]))
-    .then((data) => {
-        displayChar(data)
+    .then((data : any) => {
+        displayChar(data, charId)
     })
-      
 };
-function displayChar(data : any) {
-    char()
-    let x = 0;
-    let myChar = data.results[x];
+
+function displayChar(data : any, charId : any) {
+    const element : any = document.querySelector("tbody");
+    element.remove();
+    let myChar = data.results[charId];
     let tab = 
     `<tr>
       <th>Name</th>
-      <th>Eye Color</th>
+      <th>Hair coloe</th>
       <th>Date of Birth</th>
       <th>Eye Color</th>
      </tr>`;
-     //@ts-ignore
-     const charSpace : string = document.getElementById('info').innerHTML += tab +=
+     //const infoSpace = document.getElementById('info').innerHTML += tab +=
     `<tr>
     <td>${myChar.name}</td>
-    <td>${myChar.eye_color}</td>
+    <td>${myChar.hair_color}</td>
     <td>${myChar.date_of_birth}</td> 
     <td>${myChar.eye_color}</td> 
     </tr>`
-    console.log(myChar);
 };
-function sam(){
-    x=1
-    displayChar();
+function charButton(event : any){
+    let charId;
+    switch(event.target.name){
+        case "Frodo":
+            charId=0;
+            break;
+        case "Sam":
+            charId=1;
+            break;
+        case "Gandalf":
+            charId=2; 
+            break; 
+        case "Aragorn":
+            charId=4;
+            break;
+        case "Legolas":
+            charId=7;
+            break;
+        case "Gimli":
+            charId=8; 
+            break; 
+        case "Merry":
+            charId=5;
+            break;
+        case "Pippin":
+            charId=6;
+            break;
+        case "Boromir":
+            charId=9; 
+            break; 
+    }
+    fetchChar(charId);
+    event.document
+    event.preventDefault();
 }
-let samButton: HTMLElement | any = document.getElementById('Sam');
-samButton.addEventListener("click", sam (event) {
- event.preventDefault(),
- sam();
-});
-
-
-
-
-
+//document.querySelector('#Frodo').addEventListener('click', charButton);
+//document.querySelector('#Sam').addEventListener('click', charButton);
+//document.querySelector('#Gandalf').addEventListener('click', charButton);
+//document.querySelector('#Aragorn').addEventListener('click', charButton);
+//document.querySelector('#Legolas').addEventListener('click', charButton);
+//document.querySelector('#Gimli').addEventListener('click', charButton);
+//document.querySelector('#Merry').addEventListener('click', charButton);
+//document.querySelector('#Pippin').addEventListener('click', charButton);
+//document.querySelector('#Boromir').addEventListener('click', charButton);
+function fetchCity(cityId : any) {
+    fetch(cityUrl)
+        .then(function (response) {
+        if (response.ok) {
+            return response.json();
+        }
+        else {
+            throw new Error("NETWORK RESPONSE ERROR");
+        }
+    })
+        .then(function (data) {
+        displayCity(data, cityId);
+    });
+}
+;
+function displayCity(data : any, cityId : any) {
+    const element : any= document.querySelector("tbody");
+    element.remove();
+    var theCity = data.results[cityId];
+    var tab = `<tr>
+    <th>Name</th>
+    <th>Founding</th>
+    <th>Location</th>
+    <th>Inhabitants</th>
+   </tr>`;
+   //const infoSpace = document.getElementById('info').innerHTML += tab +=
+  `<tr>
+  <td>${theCity.name}</td>
+  <td>${theCity.founded_date}</td>
+  <td>${theCity.location}</td> 
+  <td>${theCity.inhabitants}</td> 
+  </tr>`
+}
+;
+function cityButton(event : any) {
+    var cityId;
+    switch (event.target.name) {
+        case "Minas Tirith":
+            cityId = 6;
+            break;
+        case "Annúminas":
+            cityId = 0;
+            break;
+        case "Fornost":
+            cityId = 1;
+            break;
+        case "Michel Delving":
+            cityId = 2;
+            break;
+        case "Valmar":
+            cityId = 3;
+            break;
+        case "Elvenking's Halls":
+            cityId = 4;
+            break;
+        case "Erebor":
+            cityId = 5;
+            break;
+    }
+    fetchCity(cityId);
+    event.document;
+    event.preventDefault();
+}
+//document.querySelector('#MT').addEventListener('click', cityButton);
+//document.querySelector('#Annu').addEventListener('click', cityButton);
+//document.querySelector('#Fornost').addEventListener('click', cityButton);
+//document.querySelector('#MD').addEventListener('click', cityButton);
+//document.querySelector('#Valmar').addEventListener('click', cityButton);
+//document.querySelector('#EH').addEventListener('click', cityButton);
+//document.querySelector('#Erebor').addEventListener('click', cityButton);
